73259c7f1b18147a9c49fa4af2f8b6c23465502a
Wesley <wes.beck21@outlook.com>
2021-02-03 09:55:21 -0800

    list form, controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,8 +2,19 @@ class ListsController < ApplicationController
   before_action :redirect_if_not_logged_in
   
   def index
-    @user = current_user
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @user.lists.build(list_params)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).()
 end
