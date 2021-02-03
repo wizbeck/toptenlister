8de4d2c9fce17da0533046d55531c23e9a904517
Wesley <wes.beck21@outlook.com>
2021-02-03 11:25:56 -0800

    added pry, list views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,11 +10,19 @@ class ListsController < ApplicationController
   end
 
   def create
-    @user.lists.build(list_params)
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
   end
 
   private
 
   def list_params
-    params.require(:list).()
+    params.require(:list).permit(:title, :description, :item_1, :item_2, :item_3, :item_4, :item_5, :item_6, :item_7, :item_8, :item_9, :item_10)
+  end
+
+
 end
