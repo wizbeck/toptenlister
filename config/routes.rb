316805036bc39fad9adc9898fa18ace359bbc38b
Wesley <wes.beck21@outlook.com>
2021-02-03 09:00:44 -0800

    pulled routes from broken app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#welcome'
+  
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  #login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  #logout route
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
   resources :topics
   resources :lists
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
