f17e0d5c12c136b380412c27161868df8fc3fcb1
Wesley <wes.beck21@outlook.com>
2021-02-03 16:35:56 -0800

    topics show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,3 +1,22 @@
 class TopicsController < ApplicationController
-  before_action: redirect_if_not_logged_in
+  before_action :redirect_if_not_logged_in
+
+  def show
+    binding.pry
+    @topic = Topic.find_by_id(params[:id])
+  end
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  
 end
