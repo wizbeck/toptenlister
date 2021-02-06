a73cb303b1a510dc4d0ae3f369fc25800294c184
Wesley <wes.beck21@outlook.com>
2021-02-06 10:22:19 -0800

    omniauth w/google working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,11 +24,24 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  def google
-    raise params.inspect
-    @user = User.find_or_create_by
+  def google #google oauth login for users who use google to log in to website
+    @user = User.find_or_create_by(username: auth['info']['email']) do |user|
+      user.password = SecureRandom.hex(10)
+    end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to lists_path
+    else
+      redirect_to '/'
+    end
   end
 
+
+  private 
+
+  def auth
+    request.env['omniauth.auth']
+  end
   
 
 end
