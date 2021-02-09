caeb97ec60d6170a9d2deb2b3507ea36406ae479
Wesley <wes.beck21@outlook.com>
2021-02-09 14:20:08 -0800

    fixed google auth pw val

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ has_secure_password
 has_many :lists
 has_many :topics, through: :lists
 
-#validations need to be added
 validates :username, uniqueness: true
 validates :username, length: {minimum: 6}
 
-validates :password, presence: true, length: { minimum: 5 }
+# validates :password, presence: true, length: { minimum: 5 } - look up docs for how devise overrides has_secure_password validations compared to manually setting validations in model
 
 
 end
