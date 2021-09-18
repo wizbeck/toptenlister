a60746a8952fc81332cc67ac6b6141d3a9e33168
Wesley Beck <wes.beck21@outlook.com>
2021-09-18 17:00:52 -0600

    List model update
    Added convention comments (database schema, formatting, etc.)
    Modified search method to maintain similar ordering by updated datetime.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,23 +1,39 @@
 class List < ApplicationRecord
+  # Schema Attributes
+  # "title" string
+  # "description" text
+  # "user_id" integer
+  # "topic_id" integer
+  # "item_1" string
+  # "item_2" string
+  # "item_3" string
+  # "item_4" string
+  # "item_5" string
+  # "item_6" string
+  # "item_7" string
+  # "item_8" string
+  # "item_9" string
+  # "item_10" string
+  # "created_at" datetime, precision: 6, null: false
+  # "updated_at" datetime, precision: 6, null: false
+
+  # Relationships
   belongs_to :user
   belongs_to :topic
   accepts_nested_attributes_for :topic, reject_if: proc { |attributes| attributes['name'].blank? }
-  # Validations need to be added
+  # Validations
   validates :title, presence: true
   validates :item_1, :item_2, :item_3, :item_4, :item_5, presence: true
   validates :description, length: { in: 1..60 }
-  
-  # Scope Methods 
+
+  # Scopes
   scope :order_recent, -> { order(updated_at: :desc) }
-  
+
   def self.search_lists(search)
-    where("title LIKE ?", "%#{search}%")
+    where('title LIKE ?', "%#{search}%").order_recent
   end
 
   def items
     [self.item_1, self.item_2, self.item_3, self.item_4, self.item_5, self.item_6, self.item_7, self.item_8, self.item_9, self.item_10]
   end
-
-
-
 end
