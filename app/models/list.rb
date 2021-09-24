b031363eeb3f207d93c66a04399df103f0f09933
Wesley Beck <wes.beck21@outlook.com>
2021-09-23 19:41:41 -0600

    [TODO] Documentation - models
    Added Schema information to models for easy reference during coding sessions.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,26 +1,29 @@
-class List < ApplicationRecord
-  # Schema Attributes
-  # "title" string
-  # "description" text
-  # "user_id" integer
-  # "topic_id" integer
-  # "item_1" string
-  # "item_2" string
-  # "item_3" string
-  # "item_4" string
-  # "item_5" string
-  # "item_6" string
-  # "item_7" string
-  # "item_8" string
-  # "item_9" string
-  # "item_10" string
-  # "created_at" datetime, precision: 6, null: false
-  # "updated_at" datetime, precision: 6, null: false
+# frozen_string_literal: true
 
+# Schema Information
+# "title"       :string
+# "description" :text
+# "user_id"     :integer
+# "topic_id"    :integer
+# "item_1"      :string
+# "item_2"      :string
+# "item_3"      :string
+# "item_4"      :string
+# "item_5"      :string
+# "item_6"      :string
+# "item_7"      :string
+# "item_8"      :string
+# "item_9"      :string
+# "item_10"     :string
+# "created_at"  :datetime, precision: 6, null: false
+# "updated_at"  :datetime, precision: 6, null: false
+
+class List < ApplicationRecord
   # Relationships
   belongs_to :user
   belongs_to :topic
   accepts_nested_attributes_for :topic, reject_if: proc { |attributes| attributes['name'].blank? }
+
   # Validations
   validates :title, presence: true
   validates :item_1, :item_2, :item_3, :item_4, :item_5, presence: true
@@ -34,6 +37,9 @@ class List < ApplicationRecord
   end
 
   def items
-    [self.item_1, self.item_2, self.item_3, self.item_4, self.item_5, self.item_6, self.item_7, self.item_8, self.item_9, self.item_10]
+    [
+      item_1, item_2, item_3, item_4, item_5,
+      item_6, item_7, item_8, item_9, item_10
+    ]
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,11 +1,20 @@
+# frozen_string_literal: true
+
+# Schema Information
+# "name" :string
+# "created_at" :datetime, precision: 6, null: false
+# "updated_at" :datetime, precision: 6, null: false
+
 class Topic < ApplicationRecord
+  # Relationships
   has_many :lists
   has_many :users, through: :lists
-  
+
+  # Validations
   validates :name, uniqueness: true
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
-  
-  scope :by_most_lists, -> {left_joins(:lists).group('topics.id').order('count(lists.topic_id) desc')}
-  # Topic.left_joins(:lists).group('topics.id').order('count(topics.id) desc')
+
+  # Scopes
+  scope :by_most_lists, -> { left_joins(:lists).group('topics.id').order('count(lists.topic_id) desc') }
 end
