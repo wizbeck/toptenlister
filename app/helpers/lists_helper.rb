525817627204d2d5dce5ec65d74ec6244ae0bc9d
Wesley Beck <wes.beck21@outlook.com>
2021-09-24 22:16:26 -0600

    [TODO] - minor additions
    Added flash notices that render successful CRUD actions. add flash notice to application layout to see success messages for said actions.

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,7 +1,7 @@
 module ListsHelper
 
+  # Checks if a user is creating a list from a topic page.
   def nested_topic?
     params[:topic_id] && @topic = Topic.find_by_id(params[:topic_id])
   end
-
 end
