2690ebcdef6e2147b7886d8042a972986570ff35
Wesley Beck <wes.beck21@outlook.com>
2021-10-30 18:23:17 -0600

    Move to postgresql database
    Add pg gem to allow for usage of postgresql in lieu of deploying project.
    Configure database.yml for postgresql.
    New Schema with extension for postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_03_164345) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.text "description"
